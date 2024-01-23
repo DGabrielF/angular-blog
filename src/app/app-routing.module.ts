7297c521766bf30caf0fa956617d678646ee8cd0
gabriel <dgabrielfof@gmail.com>
2024-01-23 17:27:27 -0300

    feat: getting data from a JSON

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ContentComponent } from './components/pages/content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'content/:index',
+    component:ContentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/pages/content/content.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { data } from 'src/app/data/database';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+  @Input()
+  photoCover:string="";
+  @Input()
+  contentTitle:string="";
+  @Input()
+  contentDescription:string="";
+
+  private id:string|null = "0";
+
+  constructor(private route:ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( value => {
+      this.id = value.get("index");
+    })
+    this.setValuesToContent(this.id)
+  }
+
+  setValuesToContent(id:string|null) {
+    const result = data.applications.filter(application => application.index.toString() === id)[0];
+
+    if (result) {
+      this.photoCover = result.image;
+      this.contentTitle = result.name;
+      this.contentDescription = result.description;
+    }
+  }
+
+}
